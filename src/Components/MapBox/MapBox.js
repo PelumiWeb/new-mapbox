f83a442d7e7c0d49043150851c09e4154767f8b8
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-12-15 11:24:32 -0800

    Version 2.1

--- src/Components/MapBox/MapBox.js
+++ src/Components/MapBox/MapBox.js
@@ -1,8 +1,8 @@
-import React, {useState,useEffect,useContext} from 'react'
+import React, {useState,useEffect,useContext, useRef} from 'react'
 import ReactMapGL, {Source, Layer, Marker, Popup} from 'react-map-gl'
 import axios from 'axios'
 import './MapBox.css'
-// import RoomIcon from '@material-ui/icons/Room';
+import RoomIcon from '@material-ui/icons/Room';
 import {StoreContext} from '../../store/store'
 
 import { useObserver } from 'mobx-react';
@@ -10,8 +10,10 @@ import { useObserver } from 'mobx-react';
 
 function MapBox () {
     const store = useContext(StoreContext)
+    // const mapRef = useRef()
+    // console.log(mapRef.current?.getMap())
   
-
+    const [map, setMap] = useState(null)
     const [viewport, setViewport] = useState({
         width: 480,
         height: 290,
@@ -25,6 +27,38 @@ function MapBox () {
     const [data, setData] = useState(null)
     const [selectedCoordinates, setSelectedCoordinates] = useState(null)
     const [series, setSeries] = useState(null)
+   map?.on('render', () => {
+   console.log('render')
+   })
+
+   map?.on('mousedown', 'features', function() {
+     console.log('clicked')
+   })
+   map?.on('click', 'features', function() {
+     console.log('polygon')
+   })
+
+   map?.on('load', function() {
+    // map.addSource('feature', {
+    //   'type' : 'geojson',
+    //   'data': 'geometry',
+    //   'coordinates': [-107.810134, 37.936195],
+    //                   [-107.810083, 37.936294]
+    //   type: "LineString",
+    //   properties: {},
+      // type: "Feature"
+    })
+
+    map.addLayer({
+      'id' : 'feature-layer',
+      'type' : 'line',
+      'source' : 'feature',
+      'paint' : {
+        'line-color' : 'green',
+        
+      }
+    })
+  })
    
     useEffect(() => {
         const url = "https://52-90-82-235.maverickmaven.com/geotourdata/json.cfm?h=-107,37,s,en,3A771765"
@@ -48,11 +82,11 @@ function MapBox () {
 
     return useObserver(() => (
         <React.Fragment>
-            
+            {console.log(data)}
             
              {/* data ? <div class="loader"></div> : */}
-        {/* <div>
-        <ReactMapGL 
+        {/* <div> */}
+        {/* <ReactMapGL 
       {...viewport}
       mapboxApiAccessToken={accessKey}
       mapStyle='mapbox://styles/mapbox/dark-v10'
@@ -71,9 +105,27 @@ function MapBox () {
     store.addSong(selectedCoordinates?.assets[0].audio)
     store.addImage(data.photo)
     store.addName(data.name)
-      } }>
-                 <RoomIcon />
-              </div>
+      } }> */}
+                 {/* <Source  type='geojson' data={data.geometry}>
+          
+          <Layer
+          
+            type='line'
+            layout= {{
+              'line-join': 'round',
+              'line-cap' : 'round'
+            }}
+            paint={{
+              'line-color': 'green',
+                'line-width': 8
+              }} 
+        >
+            
+              </Layer>
+        </Source>
+        Heloo
+       */}
+              {/* </div>
           </Marker>)
          )}
 
@@ -91,19 +143,28 @@ function MapBox () {
        </ReactMapGL>
        </div> */}
 
- {
+  {
        <ReactMapGL
+       onClick={{
+         'type' : 'mousedown'
+       }}
        mapStyle='mapbox://styles/mapbox/dark-v10'
        mapboxApiAccessToken={accessKey}
        onViewportChange={viewport => {
         setViewport(viewport)
     }}
        {...viewport}
-    >
-            {
+       ref={ref => ref && setMap(ref.getMap())}
+    > 
+      {/* {console.log(map)} */}
+
+    
+             {/* {
               (data && (data?.map((data, index) =>  
-        
-        <Source key={index} type='geojson' data={data.geojson}>
+        <div onClick={console.log}>
+        <Source 
+    
+        key={index} type='geojson' data={data.geojson}>
           
           <Layer
           
@@ -120,24 +181,59 @@ function MapBox () {
             
               </Layer>
         </Source>
+        </div> */}
         
-     )))}
+     {/* )))} */}
+          {/* { 
+          data && (data.map(data =>
+          <Source id='features' type='geojson' data={data?.geojson}/>
+          ))
+          }
 
-          {
-            series && (series?.map(series => 
-          <Source type='geojson' data={series.geojson}>
+          <Layer 
+          id='features'
+          source='features'
+          type='line'
+          layout= {{
+            'line-join': 'round',
+            'line-cap' : 'round'
+          }}
+          paint={{
+            'line-color': 'green',
+              'line-width': 8
+            }}
+          
+          />
+         
+
+      
+            <Source id='series' type='geojson' data={series?.[0].geojson} />
+          
          <Layer  
+         source='series'
+         id='series'
          type='fill'
          paint={{
           'fill-color': '#b8b818',
-          'fill-opacity': 0.3
+          'fill-opacity': 0.2
          }}
          /> 
-         </Source> 
-            ))
-}
-
-        </ReactMapGL> }
+            )) */}
+              {/* <Source onClick={console.log('clicked')} id='main' type='geojson' data={data?.[0].geojson}/>
+              <Layer 
+              source='main'
+              id='main'
+               type='line'
+               layout= {{
+                 'line-join': 'round',
+                 'line-cap' : 'round'
+               }}
+               paint={{
+                 'line-color': 'red',
+                   'line-width': 8
+                 }} 
+              /> */}
+        </ReactMapGL> } 
     </React.Fragment>
 
     ))
